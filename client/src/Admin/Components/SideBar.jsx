3ccdd9f9678d620ecf5837dac2d734542e8ee14c
Bishal Kayal <kayalbishal221@gmail.com>
2025-02-10 12:41:46 +0530

    Auto saved

--- client/src/Admin/Components/SideBar.jsx
+++ client/src/Admin/Components/SideBar.jsx
@@ -10,7 +10,7 @@ const SideBar = () => {
     const [currentMenu, setCurrentMenu] = useState(null);
     const [menuActive, setMenuActive] = useState(2);
     const [subMenuActive, setSubMenuActive] = useState(false);
-    const [menushow, setMenushow] = useState(false);
+    const [menushow, setMenushow] = useState(true);
 
     const MenuOpen = (menu) => {
         if (menu.submenu) {
@@ -24,11 +24,11 @@ const SideBar = () => {
 
     return (
         <>
-            <aside className={`${menushow ? 'w-60' : 'w-20'} bg-[#0e0e23] h-full fixed z-[103] transition-all duration-[0.05s] ease-[ease]`}>
-                <div className={`${menushow ? 'w-60' : 'w-20'} fixed flex z-[9] items-center justify-center transition-all duration-[0.05s] ease-[ease] px-5 py-5 border-e border-y border-[#ffffff1a]`}>
-                    <Link to={''}>
-                        <img src={Images.BrandWhite} className={menushow ? 'w-[184px]' : 'hidden'} alt="" />
-                        <img src={Images.SmallLogoWhite} className={menushow ? 'hidden' : 'w-[184px]'} alt="" />
+            <aside className={`${menushow ? 'w-60' : 'w-20 hover:w-60'} bg-[#0e0e23] h-full group fixed z-[103] transition-all duration-[0.3s] ease-[ease]`}>
+                <div className={`${menushow ? 'w-60' : 'w-20 group-hover:w-60'} fixed flex z-[9] items-center justify-center transition-all duration-[0.3s] ease-[ease] px-5 py-5 border-e border-y border-[#ffffff1a]`}>
+                    <Link to={''} className=''>
+                        <img src={Images.BrandWhite} className={`${menushow ? 'w-[184px]' : 'hidden group-hover:block'} transition-all duration-[0.3s]`} alt="" />
+                        <img src={Images.SmallLogoWhite} className={`${menushow ? 'hidden' : 'w-[184px] group-hover:hidden'} transition-all duration-[0.3s]`} alt="" />
                     </Link>
                 </div>
 
@@ -39,27 +39,27 @@ const SideBar = () => {
                                 return (
                                     <>
                                         {menu.cat === 'heading' ? (
-                                            <li key={menu.id} className={menushow ? 'text-[#ffffff4d] text-[0.718rem] font-medium uppercase opacity-80 relative pt-3 pb-[0.2rem] px-[0.93rem]' : 'hidden'}>
+                                            <li key={menu.id} className={`${menushow ? 'block' : 'hidden group-hover:block'} text-[#ffffff4d] text-[0.718rem] font-medium uppercase opacity-80 relative pt-3 pb-[0.2rem] px-[0.93rem]`}>
                                                 <span>{menu.name}</span>
                                             </li>
                                         ) :
                                             (
                                                 <li key={menu.id} onClick={() => MenuOpen(menu)} className={menuActive === menu.id ? 'mt-[5px] p-0' : 'my-[5px]'}>
-                                                    <Link to={''} className={ !menushow && (menuActive === menu.id) ? 'my-[15px] h-[43px] bg-[#eaedf7] p-[0.25rem] relative z-[1] mr-auto text-center rounded-full border-[0] border-transparent w-fit flex text-[.875rem] text-[#fff6] items-center no-underline font-normal before:absolute before:top-[-30px] before:z-[9] before:h-[30px] before:border-l-transparent before:border-t-transparent before:border-b-[transparent] before:bottom-0 before:rounded-br-[48px] before:border-e-[20px] before:right-0 before:border-[#0e0e23] after:content-[""] after:absolute after:z-0 after:h-[30px] after:border-l-transparent after:border-t-transparent after:border-b-[transparent] after:top-[43px] after:bottom-0 after:right-0 after:rounded-tl-none after:rounded-tr-[48px] after:rounded-bl-none after:rounded-br-none after:border-e-[20px] after:border-[#0e0e23]' : ( menushow && menuActive === menu.id ? 'my-[15px] h-[43px] bg-[#eaedf7] p-[0.25rem] relative z-[1] mx-auto text-center rounded-tl-[30px] rounded-bl-[30px] border-[0] border-transparent w-full flex text-[.875rem] text-[#fff6] items-center no-underline font-normal before:absolute before:top-[-30px] before:z-[9] before:h-[30px] before:border-l-transparent before:border-t-transparent before:border-b-[transparent] before:bottom-0 before:rounded-br-[48px] before:border-e-[20px] before:right-0 before:border-[#0e0e23] after:content-[""] after:absolute after:z-0 after:h-[30px] after:border-l-transparent after:border-t-transparent after:border-b-[transparent] after:top-[43px] after:bottom-0 after:right-0 after:rounded-tl-none after:rounded-tr-[48px] after:rounded-bl-none after:rounded-br-none after:border-e-[20px] after:border-[#0e0e23]' : 'relative group flex items-center no-underline text-[.875rem] text-[#fff6] font-normal p-3 rounded-[0.688rem]')}>
-                                                        <span className={menushow ? (menuActive === menu.id ? 'absolute top-[-30px] w-5 h-[60px] right-0 bg-[#eaedf7] ' : 'hidden') : 'hidden'}></span>
-                                                        <span className={menushow ? (menuActive === menu.id ? 'absolute top-[35px] w-5 h-[30px] right-0 bg-[#eaedf7] ' : 'hidden') : 'hidden'}></span>
-                                                        <span className={ !menushow && (menuActive === menu.id) ? 'text-lg w-[35px] h-[35px] leading-[33px] text-center shadow-[0_5px_10px_#0003] text-white rounded-[50%] bg-[#6259ca] flex items-center justify-center' : (menuActive === menu.id ? 'text-lg w-[35px] h-[35px] leading-[33px] text-center shadow-[0_5px_10px_#0003] text-white rounded-[50%] bg-[#6259ca] flex items-center justify-center mr-[10px]' : 'flex items-center transition-all duration-300 justify-center mr-[1.2rem] w-[1.15rem] h-[1.15rem] text-[1.063rem] text-center group-hover:fill-white text-[#fff6] group-hover:text-white fill-[#fff6] rounded-[0.688rem] hover:font-medium')}>{menu.icon}</span>
-                                                        <span className={ !menushow ? 'hidden' : (menuActive === menu.id ? 'text-[#6259ca] font-normal relative text-[.875rem] leading-[1]' : 'whitespace-nowrap transition-all duration-300 group-hover:text-white text-[#fff6] relative text-sm leading-none align-middle hover:font-medium')}>{menu.name}</span>
+                                                    <Link to={''} className={!menushow && (menuActive === menu.id) ? 'my-[15px] group-hover:w-full h-[43px] bg-[#eaedf7] p-[0.25rem] relative z-[1] mr-auto text-center rounded-full border-[0] border-transparent w-fit flex text-[.875rem] text-[#fff6] items-center no-underline font-normal before:absolute before:top-[-30px] before:z-[9] before:h-[30px] before:border-l-transparent before:border-t-transparent before:border-b-[transparent] before:bottom-0 before:rounded-br-[48px] before:border-e-[20px] before:right-0 before:border-[#0e0e23] after:content-[""] after:absolute after:z-0 after:h-[30px] after:border-l-transparent after:border-t-transparent after:border-b-[transparent] after:top-[43px] after:bottom-0 after:right-0 after:rounded-tl-none after:rounded-tr-[48px] after:rounded-bl-none after:rounded-br-none after:border-e-[20px] after:border-[#0e0e23]' : (menushow && menuActive === menu.id ? 'my-[15px] h-[43px] bg-[#eaedf7] p-[0.25rem] relative z-[1] mx-auto text-center rounded-tl-[30px] rounded-bl-[30px] border-[0] border-transparent w-full flex text-[.875rem] text-[#fff6] items-center no-underline font-normal before:absolute before:top-[-30px] before:z-[9] before:h-[30px] before:border-l-transparent before:border-t-transparent before:border-b-[transparent] before:bottom-0 before:rounded-br-[48px] before:border-e-[20px] before:right-0 before:border-[#0e0e23] after:content-[""] after:absolute after:z-0 after:h-[30px] after:border-l-transparent after:border-t-transparent after:border-b-[transparent] after:top-[43px] after:bottom-0 after:right-0 after:rounded-tl-none after:rounded-tr-[48px] after:rounded-bl-none after:rounded-br-none after:border-e-[20px] after:border-[#0e0e23] ' : 'relative group/link flex items-center no-underline text-[.875rem] text-[#fff6] font-normal p-3 rounded-[0.688rem]')}>
+                                                        <span className={!menushow && (menuActive === menu.id) ? 'group-hover:absolute group-hover:top-[-30px] group-hover:w-5 group-hover:h-[80px] group-hover:right-0 group-hover:bg-[#eaedf7]' : (menushow ? (menuActive === menu.id ? 'absolute top-[-30px] w-5 h-[60px] right-0 bg-[#eaedf7] ' : 'hidden') : 'hidden')}></span>
+                                                        <span className={!menushow && (menuActive === menu.id) ? 'group-hover:absolute group-hover:top-[35px] group-hover:w-5 group-hover:h-[30px] group-hover:right-0 group-hover:bg-[#eaedf7] ' : (menushow ? (menuActive === menu.id ? 'absolute top-[35px] w-5 h-[30px] right-0 bg-[#eaedf7] ' : 'hidden') : 'hidden')}></span>
+                                                        <span className={!menushow && (menuActive === menu.id) ? 'group-hover:mr-[10px] text-lg w-[35px] h-[35px] leading-[33px] text-center shadow-[0_5px_10px_#0003] text-white rounded-[50%] bg-[#6259ca] flex items-center justify-center' : (menuActive === menu.id ? 'text-lg w-[35px] h-[35px] leading-[33px] text-center shadow-[0_5px_10px_#0003] text-white rounded-[50%] bg-[#6259ca] flex items-center justify-center mr-[10px]' : 'flex items-center transition-all duration-300 justify-center mr-[1.2rem] w-[1.15rem] h-[1.15rem] text-[1.063rem] text-center group-hover:fill-white text-[#fff6] group-hover/link:text-white fill-[#fff6] rounded-[0.688rem] hover:font-medium')}>{menu.icon}</span>
+                                                        <span className={!menushow && (menuActive !== menu.id) ? 'group-hover/link:text-white group-hover:block hidden' : (!menushow ? 'group-hover:block hidden group-hover:text-[#6259ca]' : (!menushow && (menuActive === menu.id) ? 'hidden group-hover:block text-[#6259ca] group-hover:font-normal group-hover:relative group-hover:text-[.875rem] group-hover:leading-[1]' : (menuActive === menu.id ? 'text-[#6259ca] font-normal relative text-[.875rem] leading-[1]' : 'whitespace-nowrap transition-all duration-300 group-hover/link:text-white text-[#fff6] relative text-sm leading-none align-middle hover:font-medium')))}>{menu.name}</span>
                                                     </Link>
                                                     {(menu.submenu && subMenuActive && currentMenu === menu.id) &&
-                                                        <ul className='pl-[1.2rem]'>
+                                                        <ul className={menushow ? 'pl-[1.2rem]' : ''}>
                                                             {menu.submenu.map((submenu, i) => {
                                                                 return (
                                                                     <>
                                                                         <li key={menu.id} onClick={() => setMenuActive(menu.id)}>
-                                                                            <Link to={''} className='py-[0.45rem] pr-[1.6rem] hover:text-white hover:font-medium transition-all duration-300 text-[#fff6] inline-flex items-start'>
-                                                                                <span className='mr-[1rem] mt-[0.15rem]'><MdKeyboardDoubleArrowRight /></span>
-                                                                                <span className='text-[0.8rem]'>{submenu.name}</span>
+                                                                            <Link to={''} className={menushow ? 'py-[0.45rem] pr-[1.6rem] hover:text-white hover:font-medium transition-all duration-300 text-[#fff6] inline-flex items-start' : 'py-[0.45rem] pr-[0.5rem] pl-[0.6rem] hover:text-white hover:font-medium transition-all duration-300 text-[#fff6] inline-flex items-start'}>
+                                                                                <span className={menushow ? 'mr-[1rem] mt-[0.15rem]' : 'mt-[0.15rem] group-hover:mr-[1rem]'}><MdKeyboardDoubleArrowRight /></span>
+                                                                                <span className={`text-[0.8rem] ${menushow ? '' : 'hidden group-hover:block'}`}>{submenu.name}</span>
                                                                             </Link>
                                                                         </li>
                                                                     </>
@@ -72,14 +72,6 @@ const SideBar = () => {
                                     </>
                                 )
                             })}
-                            <li className='mt-[5px] p-0'>
-                                <Link to={''} className='my-[15px] h-[43px] bg-[#eaedf7] p-[0.25rem] relative z-[1] mr-auto text-center rounded-full border-[0] border-transparent w-fit flex text-[.875rem] text-[#fff6] items-center no-underline font-normal before:absolute before:top-[-30px] before:z-[9] before:h-[30px] before:border-l-transparent before:border-t-transparent before:border-b-[transparent] before:bottom-0 before:rounded-br-[48px] before:border-e-[20px] before:right-0 before:border-[#0e0e23] after:content-[""] after:absolute after:z-0 after:h-[30px] after:border-l-transparent after:border-t-transparent after:border-b-[transparent] after:top-[43px] after:bottom-0 after:right-0 after:rounded-tl-none after:rounded-tr-[48px] after:rounded-bl-none after:rounded-br-none after:border-e-[20px] after:border-[#0e0e23]'>
-                                    <span className='hidden absolute top-[-30px] w-5 h-[60px] right-0 bg-[#eaedf7] '></span>
-                                    <span className='hidden absolute top-[35px] w-5 h-[30px] right-0 bg-[#eaedf7] '></span>
-                                    <span className='text-lg w-[35px] h-[35px] leading-[33px] text-center shadow-[0_5px_10px_#0003] text-white rounded-[50%] bg-[#6259ca] flex items-center justify-center'><MdKeyboardDoubleArrowRight /></span>
-                                    <span className='hidden text-[#6259ca] font-normal relative text-[.875rem] leading-[1]'>Dashboard</span>
-                                </Link>
-                            </li>
                         </ul>
                     </nav>
                 </div>
